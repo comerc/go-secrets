7c92ed1a36a1f1223e6b7227b20aa2762d005e7b
comerc <andrew.kachanov@gmail.com>
2025-03-16 13:48:47 +0300

    vault backup: 2025-03-16 13:48:47

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module go-secrets
 
-go 1.24.1
+go 1.23.4
--- try/try_test.go
+++ try/try_test.go
@@ -11,12 +11,13 @@ func Test2(t *testing.T) {
 	fmt.Println("123")
 	println("123")
 	synctest.Run(func() {
-		before := time.Now()
+		// before := time.Now()
 		time.Sleep(time.Second)
-		after := time.Now()
-		if d := after.Sub(before); d != time.Second {
-			t.Fatalf("took %v", d)
-		}
-		t.Log("done")
+		// after := time.Now()
+		// if d := after.Sub(before); d != time.Second {
+		// 	t.Fatalf("took %v", d)
+		// }
+		t.Fatalf("took %v", 1)
+		// t.Log("done")
 	})
 }
