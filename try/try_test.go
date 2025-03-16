a8a398dd32df18b19e6c9379fa09a0629508e129
comerc <andrew.kachanov@gmail.com>
2025-03-16 12:09:21 +0300

    vault backup: 2025-03-16 12:09:21

---
+++ try/try_test.go
@@ -0,0 +1,16 @@
+import (
+	"testing"
+	"testing/synctest"
+	"time"
+)
+
+func Test(t *testing.T) {
+	synctest.Run(func() {
+		before := time.Now()
+		time.Sleep(time.Second)
+		after := time.Now()
+		if d := after.Sub(before); d != time.Second {
+			t.Fatalf("took %v", d)
+		}
+	})
+}
